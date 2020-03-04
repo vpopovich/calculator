bf6f86c7f2178ea4f36697dd69151af7e57e0deb
Vladimir Popovich <vladimir.popovich@exyte.com>
2020-03-04 17:08:26 +0700

    Remove dependency from mathjs

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import { Button } from './components/Button.js';
 import { Input } from './components/Input';
 import { ClearButton } from './components/ClearButton';
-import * as math from 'mathjs';
+
 
 class App extends Component {
   constructor(props) {
@@ -19,7 +19,72 @@ class App extends Component {
   }
 
   handleEqual = () => {
-    this.setState({ input: math.eval(this.state.input) });
+    this.setState({ input: this.calculate(this.state.input) });
+  }
+
+  calculate = (input) => {
+
+      var f = {
+      add: '+',
+      sub: '-',
+      div: '/',
+      mlt: '*',
+      mod: '%',
+      exp: '^'
+    };
+  
+    // Create array for Order of Operation and precedence
+    f.ooo = [
+      [
+        [f.mlt],
+        [f.div]
+      ],
+      [
+        [f.add],
+        [f.sub]
+      ]
+    ];
+  
+    input = input.replace(/[^0-9%^*\/()\-+.]/g, ''); // clean up unnecessary characters
+  
+    var output;
+    for (var i = 0, n = f.ooo.length; i < n; i++) {
+  
+      // Regular Expression to look for operators between floating numbers or integers
+      var re = new RegExp('(\\d+\\.?\\d*)([\\' + f.ooo[i].join('\\') + '])(\\d+\\.?\\d*)');
+      re.lastIndex = 0; // take precautions and reset re starting pos
+  
+      // Loop while there is still calculation for level of precedence
+      while (re.test(input)) {
+        output = _calculate(RegExp.$1, RegExp.$2, RegExp.$3);
+        if (isNaN(output) || !isFinite(output)) 
+          return output; // exit early if not a number
+        input = input.replace(re, output);
+      }
+    }
+  
+    return output;
+  
+    function _calculate(a, op, b) {
+      a = a * 1;
+      b = b * 1;
+      switch (op) {
+        case f.add:
+          return a + b;
+          break;
+        case f.sub:
+          return a - b;
+          break;
+        case f.div:
+          return a / b;
+          break;
+        case f.mlt:
+          return a * b;
+          break;
+        default:
+          return;
+      }
+    }
   }
 
   render() {
